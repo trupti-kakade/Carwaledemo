ac59d46f9b61b38cf9baff572f5d465746aa0794
PranaliKulal <pranali.kulal@gmail.com>
2023-04-16 11:55:14 +0530

    Sixth

--- src/main/java/com/carwaledemo/base/A.java
+++ src/main/java/com/carwaledemo/base/A.java
@@ -2,6 +2,7 @@ package com.carwaledemo.base;
 
 public class A {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello ");
+		System.out.println("Hello Java");
 	}
 }
